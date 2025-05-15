89d7d0de1c1689f5d5ab46dafae3839062286df7
KarolPuczynski <karol.puczynski123@gmail.com>
2025-05-15 12:42:47 +0200

    1

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ while True:
     
     drawing_chessboard(screen)
     draw_pieces(screen)
-    # event checking
-    # and i dont know what else
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
